b9ed36b7c3ab24ef30eb03fa821ad7145d966a80
Alex Junior <alex.s.jr@outlook.com>
2022-10-21 16:32:59 -0300

    Criada a base de dados h2 com sucesso

--- src/main/java/com/locadora/App.java
+++ src/main/java/com/locadora/App.java
@@ -1,13 +1,21 @@
 package com.locadora;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.locadora.services.UserService;
 
-@SpringBootApplication
+@SpringBootApplication()
 @RestController
 public class App{
 
